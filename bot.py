5fa48aeafc668c481cf1725694a485f19e6aa94c
unknown <jiaqifen@buffalo.edu>
2021-02-15 22:16:11 -0500

    bot setup

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
 import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
+client = discord.Client()
+
+
+@client.event
+async def on_message(msg):
+    if msg.content == "hello" or msg.content == "hi":
+        await msg.channel.send("hewwo~")
+
+
+client.run(TOKEN)
